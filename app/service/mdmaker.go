f1fe0e7e33c9e735684af8e555e61f038ce54f83
leo <anlitylee@qq.com>
2022-12-29 13:45:48 +0800

    feat: Enrich the markdown syntax

--- app/service/mdmaker.go
+++ app/service/mdmaker.go
@@ -1,5 +1,7 @@
 package service
 
+import "github.com/gogf/gf/util/gconv"
+
 var MdMaker = &mdMakerService{}
 
 type mdMakerService struct {
@@ -56,6 +58,58 @@ func (s *mdMakerService) Code(data string) (re string) {
 	return re + "\n"
 }
 
+func (s *mdMakerService) RowCode(data string) (re string) {
+	return "```" + data + "```"
+}
+
 func (s *mdMakerService) Quote(data string) (re string) {
 	return "> " + data + "\n"
+}
+
+func (s *mdMakerService) Color(data string, color string) (re string) {
+	return "<font color=" + color + ">" + data + "</font>"
+}
+
+func (s *mdMakerService) Bold(data string) (re string) {
+	return "**" + data + "**"
+}
+
+func (s *mdMakerService) Italic(data string) (re string) {
+	return "*" + data + "*"
+}
+
+func (s *mdMakerService) BoldAndItalic(data string) (re string) {
+	return "***" + data + "***"
+}
+
+func (s *mdMakerService) Lists(data []string, order ...bool) (re string) {
+	re = ""
+	for key, item := range data {
+		if len(order) > 0 && order[0] {
+			re += gconv.String(key+1) + ". " + item + "\n"
+		} else {
+			re += "- " + item + "\n"
+		}
+	}
+	return re
+}
+
+func (s *mdMakerService) HorizontalRule() (re string) {
+	return "\n---\n\n"
+}
+
+func (s *mdMakerService) Link(title, link string) (re string) {
+	return "[" + title + "](" + link + ")"
+}
+
+func (s *mdMakerService) Url(data string) (re string) {
+	return "<" + data + ">"
+}
+
+func (s *mdMakerService) Image(imagePath, altText string, link ...string) (re string) {
+	if len(link) > 0 && link[0] != "" {
+		return "[![" + altText + "](" + imagePath + ")](" + link[0] + ")"
+	} else {
+		return "![" + altText + "](" + imagePath + ")"
+	}
 }
